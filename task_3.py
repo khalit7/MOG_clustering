fixed the covariance singularity problem using three different methods

task_3.py
@@ -32,12 +32,13 @@ X_full = np.zeros((len(f1), 2))
 #########################################
 # Write your code here
 # Store f1 in the first column of X_full, and f2 in the second column of X_full
-
+X_full[0:,0] += f1
+X_full[0:,1] += f2 
 ########################################/
 X_full = X_full.astype(np.float32)
 
 # number of GMM components
-k = 3
+k = 3 # Or 6
 
 #########################################
 # Write your code here
@@ -46,9 +47,9 @@ k = 3
 # The shape of X_phonemes_1_2 will be two-dimensional. Each row will represent a sample of the dataset, and each column will represent a feature (e.g. f1 or f2)
 # Fill X_phonemes_1_2 with the samples of X_full that belong to the chosen phonemes
 # To fill X_phonemes_1_2, you can leverage the phoneme_id array, that contains the ID of each sample of X_full
-
-# X_phonemes_1_2 = ...
-
+phoneme_1_indices = np.where(phoneme_id==1)[0]
+phoneme_2_indices = np.where(phoneme_id==2)[0]
+X_phonemes_1_2 =X_full[np.append(phoneme_1_indices,phoneme_2_indices)]
 ########################################/
 
 # Plot array containing the chosen phonemes
@@ -69,7 +70,26 @@ plt.savefig(plot_filename)
 # Get predictions on samples from both phonemes 1 and 2, from a GMM with k components, pretrained on phoneme 1
 # Get predictions on samples from both phonemes 1 and 2, from a GMM with k components, pretrained on phoneme 2
 # Compare these predictions for each sample of the dataset, and calculate the accuracy, and store it in a scalar variable named "accuracy"
+model_npy_file = 'data/GMM_params_phoneme_0{ph}_k_0{k}.npy'
+X =X_phonemes_1_2.copy()
+# first model's predections
+model_ph1 =np.ndarray.tolist(np.load(model_npy_file.format(ph=1, k=k), allow_pickle=True))
+Z_model_ph1 = np.sum ( get_predictions(model_ph1['mu'], model_ph1['s'], model_ph1['p'], X),axis=1)
 
+# second model's predections
+model_ph2 =np.ndarray.tolist(np.load(model_npy_file.format(ph=2, k=k), allow_pickle=True))
+Z_model_ph2 = np.sum(get_predictions(model_ph2['mu'], model_ph2['s'], model_ph2['p'], X),axis=1)
+
+preds = Z_model_ph1>Z_model_ph2
+y_truth = np.append(np.ones(phoneme_1_indices.size),np.zeros(phoneme_2_indices.size))
+
+# compute accuracy manually, (I wanted to use sklearn's function accuracy_score but was worried that marker may not have the library installled)
+counter=0
+for i in range(preds.size):
+    if (y_truth[i]==preds[i]):
+        counter+=1
+
+accuracy = counter/preds.size
 ########################################/
 
 print('Accuracy using GMMs with {} components: {:.2f}%'.format(k, accuracy))

task_4.py
@@ -32,7 +32,8 @@ X_full = np.zeros((len(f1), 2))
 #########################################
 # Write your code here
 # Store f1 in the first column of X_full, and f2 in the second column of X_full
-
+X_full[0:,0] += f1
+X_full[0:,1] += f2 
 ########################################/
 X_full = X_full.astype(np.float32)
 
@@ -47,7 +48,9 @@ k = 3
 # Fill X_phonemes_1_2 with the samples of X_full that belong to the chosen phonemes
 # To fill X_phonemes_1_2, you can leverage the phoneme_id array, that contains the ID of each sample of X_full
 
-# X_phonemes_1_2 = ...
+phoneme_1_indices = np.where(phoneme_id==1)[0]
+phoneme_2_indices = np.where(phoneme_id==2)[0]
+X_phonemes_1_2 =X_full[np.append(phoneme_1_indices,phoneme_2_indices)]
 
 ########################################/
 
@@ -75,7 +78,33 @@ print('f2 range: {}-{} | {} points'.format(min_f2, max_f2, N_f2))
 # Store these prediction in a 2D numpy array named "M", of shape N_f2 x N_f1 (the first dimension is f2 so that we keep f2 in the vertical axis of the plot)
 # M should contain "0.0" in the points that belong to phoneme 1 and "1.0" in the points that belong to phoneme 2
 ########################################/
+grid = []
+f2_values = [*range(min_f2,max_f2)]
+for f1_value in range(min_f1,max_f1):
+    temp = []
+    for f2_value in range(min_f2,max_f2):
+        temp.append([f1_value,f2_value])
+    grid.append(temp)
+grid = np.array(grid)
 
+model_npy_file = 'data/GMM_params_phoneme_0{ph}_k_0{k}.npy'
+model_ph1 =np.ndarray.tolist(np.load(model_npy_file.format(ph=1, k=k), allow_pickle=True))
+model_ph2 =np.ndarray.tolist(np.load(model_npy_file.format(ph=2, k=k), allow_pickle=True))
+ph1_predictions= np.zeros((grid.shape[0],grid.shape[1],k))
+ph2_predictions= np.zeros((grid.shape[0],grid.shape[1],k))
+for i in range(grid.shape[0]):
+    ph1_predictions[i] = get_predictions(model_ph1['mu'], model_ph1['s'], model_ph1['p'], grid[i])
+    ph2_predictions[i] = get_predictions(model_ph2['mu'], model_ph2['s'], model_ph2['p'], grid[i])
+
+
+M= np.zeros((grid.shape[0],grid.shape[1]))
+for i in range(grid.shape[0]):
+    for j in range(grid.shape[1]):
+        if (np.sum(ph1_predictions[i][j])>np.sum(ph2_predictions[i][j])):
+            M[i][j] =1
+        else:
+            M[i][j] = 2
+M=np.transpose(M)
 ################################################
 # Visualize predictions on custom grid
 

task_5.py
@@ -34,7 +34,9 @@ X_full = np.zeros((len(f1), 3))
 #########################################
 # Write your code here
 # Store f1 in the first column of X_full, f2 in the second column of X_full and f1+f2 in the third column of X_full
-
+X_full[0:,0] += f1
+X_full[0:,1] += f2 
+X_full[0:,2] += f1+f2
 ########################################/
 X_full = X_full.astype(np.float32)
 
@@ -51,8 +53,8 @@ k = 3
 # The shape of X_phoneme will be two-dimensional. Each row will represent a sample of the dataset, and each column will represent a feature (e.g. f1 or f2 or f1+f2)
 # Fill X_phoneme with the samples of X_full that belong to the chosen phoneme
 # To fill X_phoneme, you can leverage the phoneme_id array, that contains the ID of each sample of X_full
-
-# X_phoneme = ...
+phoneme_indices = np.where(phoneme_id==p_id)[0]
+X_phoneme = X_full[phoneme_indices]
 
 ########################################/
 
@@ -127,7 +129,15 @@ for t in range(n_iter):
         #########################################
         # Write your code here
         # Suggest ways of overcoming the singularity
-
+        # First suggestion: let the covaraince matrix be a diagonal matrix with values sigma (all elements along the diagonal have the same value)
+        '''sigma_square = np.sum(term_1.transpose()@term_1)/N #scaler
+        s[i,:,:] = np.sqrt(sigma_square)*np.identity(D)'''
+        # Second suggestion : let the covariance matrix be a diagonal matrix with different sigma values
+        '''sigma_square = np.sum(np.square(term_1),axis=1) # has shape (D,)
+        sigma_diag = np.diag(np.sqrt(sigma_square))
+        s[i,:,:] = sigma_diag'''
+        # Third suggestion: add a smal positive value to the diagonal of the covariance matrix
+        '''s[i,:,:]+= 0.000001*np.identity(D)'''
         ########################################/
         p[i] = np.mean(Z[:,i])
     ax1.clear()